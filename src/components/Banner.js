fca9b3bb5213b5d3a06c145abeb0967ce0fa17c4
Jose Tobar <josectobar@gmail.com>
2019-04-09 19:31:35 -0600

    intial commit - portfolio

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,58 @@
 import React from 'react'
 
-const Banner = (props) => (
-    <section id="banner" className="major">
+const Banner = props => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
+        <h1>
+          JOSE TOBAR <br />
+          Web Developer
+        </h1>
         <div className="inner">
-            <header className="major">
-                <h1>Hi, my name is Forty</h1>
-            </header>
-            <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
-                </ul>
-            </div>
+          <ul className="icons">
+            <li>
+              <a
+                href="https://twitter.com/josectobar"
+                className="icon alt fa-twitter"
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/josectobar"
+                className="icon alt fa-github"
+              >
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/josectobar/"
+                className="icon alt fa-linkedin"
+              >
+                <span className="label">LinkedIn</span>
+              </a>
+            </li>
+          </ul>
         </div>
-    </section>
+      </header>
+      <div className="content">
+        <p>
+          Creative and motived individual, passionate about the latest
+          <br />
+          web development trends to create awesome web applications.
+        </p>
+        {/* <ul className="actions">
+          <li>
+            <a href="#one" className="button next scrolly">
+              Learn more
+            </a>
+          </li>
+        </ul> */}
+      </div>
+    </div>
+  </section>
 )
 
 export default Banner
