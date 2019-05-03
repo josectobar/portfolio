1212ed62980fed8542df422b320ea917803e79ea
Jose Tobar <josectobar@gmail.com>
2019-05-03 00:18:47 -0600

    minor fixes

--- src/pages/dayhealth.js
+++ src/pages/dayhealth.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import BannerDayHealth from '../components/BannerDayHealth'
@@ -7,8 +6,6 @@ import BannerDayHealth from '../components/BannerDayHealth'
 import dashboard from '../assets/images/dashboard.png'
 import wizard from '../assets/images/wizard.png'
 import stats from '../assets/images/stats.png'
-import pic09 from '../assets/images/pic09.jpg'
-import pic10 from '../assets/images/pic10.jpg'
 
 const Landing = props => (
   <Layout>
@@ -32,10 +29,10 @@ const Landing = props => (
           <p>
             Frontend built with <strong>React</strong> , <strong>Redux</strong>{' '}
             was used for most of the data flow functionality; Styled with{' '}
-            <strong>SASS</strong>. The backend runs on <strong>Nodejs</strong>,
+            <strong>SASS</strong>. The backend runs on <strong>NodeJS</strong>,
             the server is based on <strong>ExpressJS</strong> and the{' '}
-            <strong>PostgreSQL</strong>
-            database is connected through <strong>Massive</strong>.
+            <strong>PostgreSQL</strong> database is connected through{' '}
+            <strong>Massive</strong>.
             <br />
             {`Please refer to `}
             <a
@@ -60,10 +57,14 @@ const Landing = props => (
                 <h3>Dashboard</h3>
               </header>
               <p>
-              In the dashboard, the logged in user can view previously-entered logs under the “previous entries” section on the lower side of the page, 
-              the previous entries can be viewed, edited and deleted. To view an entry, the user can click on the entry title or select the "view" option 
-              from the drop-down menu on the right. They can also read a health-related article by clicking the image or the “learn more” button that will 
-              open a separate tab to an external page.
+                In the dashboard, the logged in user can view existing logs
+                under the “previous entries” section on the lower side of the
+                page, the previous entries can be viewed, edited and deleted. To
+                view an entry, the user can click on the entry title or select
+                the "view" option from the drop-down menu on the right. They can
+                also read a health-related article by clicking the image or the
+                “learn more” button that will open a separate tab to an external
+                page.
               </p>
             </div>
           </div>
@@ -78,8 +79,11 @@ const Landing = props => (
                 <h3>Statistics section</h3>
               </header>
               <p>
-              A line graph for the pain and overall conditions readings will be displayed in the dashboard as long as there is data entered. When clicked,
-               the graph will route to the statistics page. Users can view graphs and add readings if they click the "add indicators" button.
+                A line graph for the pain and overall conditions readings will
+                be displayed in the dashboard as long as there is data entered.
+                When clicked, the graph will route to the statistics page. Users
+                can view graphs and add readings if they click the "add
+                indicators" button.
               </p>
             </div>
           </div>
@@ -94,9 +98,12 @@ const Landing = props => (
                 <h3>Wizard</h3>
               </header>
               <p>
-              On the first step in the wizard, the user is able to add their current health condition based on any of the four different indicators available: 
-              overall health, pain level, glucose level, and blood pressure. After clicking next, they are required to enter a log before submitting. 
-              Throughout the wizard, the user is able to cancel and will then be then routed to the dashboard.
+                On the first step in the wizard, the user is able to add their
+                current health condition based on any of the four different
+                indicators available: overall health, pain level, glucose level,
+                and blood pressure. After clicking next, they are required to
+                enter a log before submitting. Throughout the wizard, the user
+                is able to cancel and will then be then routed to the dashboard.
               </p>
             </div>
           </div>
